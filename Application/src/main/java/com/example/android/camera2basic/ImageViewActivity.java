a4f8fc702d2886f4d629544d697563e2d9e9cb3d
soundofhorizon <ooshiba0206@gmail.com>
2021-08-23 15:32:46 +0900

    [UPDATE]OCR機能追加

--- Application/src/main/java/com/example/android/camera2basic/ImageViewActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/ImageViewActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,12 +17,33 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequest;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.api.services.vision.v1.model.TextAnnotation;
+
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 public class ImageViewActivity extends AppCompatActivity {
 
@@ -32,6 +55,17 @@ public class ImageViewActivity extends AppCompatActivity {
     // asset の画像ファイル名
     private final String fileName = "pic.jpg";
     private File file;
+
+    // Google Cloud Visionの各種設定
+
+    //API keyを直書きしている。この間はgithubなどを他人に見せる、他人にコードを譲渡するなどの行為一切を禁ずる。
+    //TODO: API KEYをどこかの環境変数として早く格納するべき
+    private static final String CLOUD_VISION_API_KEY = "AIzaSyBSD-HrXEvueW_SzFUa11q7NY9tv10Mp-o";
+    private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
+    private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
+    private static final int MAX_LABEL_RESULTS = 1;
+
+    private static final String TAG = ImageViewActivity.class.getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,9 +80,9 @@ public class ImageViewActivity extends AppCompatActivity {
         Log.d("log","path: " + file);
 
         // text_view： activity_main.xml の TextView の id
-        TextView textView = findViewById(R.id.text_view);
-        // テキストを設定
-        textView.setText(R.string.description_for_read_image);
+        TextView mImageDetails = findViewById(R.id.text_view);
+        // テキストを設定。画像更新後、OCR用のString変数として利用。
+        mImageDetails.setText(R.string.description_for_read_image);
 
         setUpWriteExternalStorage();
     }
@@ -59,8 +93,9 @@ public class ImageViewActivity extends AppCompatActivity {
             if(isExternalStorageReadable()){
                 try(InputStream inputStream0 =
                             new FileInputStream(file) ) {
-
                     Bitmap bitmap = BitmapFactory.decodeStream(inputStream0);
+                    // 生成したbitmapをuploadする。
+                    uploadImage(bitmap);
                     imageView1.setImageBitmap(bitmap);
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -69,10 +104,162 @@ public class ImageViewActivity extends AppCompatActivity {
         });
     }
 
-    /* Checks if external storage is available for read and write */
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        return (Environment.MEDIA_MOUNTED.equals(state));
+    public void uploadImage(Bitmap bitmap){
+        if (bitmap!= null) {
+            // OCR処理実行中であることを伝える。
+            TextView description = findViewById(R.id.text_view);
+            description.setText(R.string.loading_ocr);
+            callCloudVision(bitmap);
+        } else {
+            Log.d(TAG, "Image picker gave us a null image.");
+            Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void callCloudVision(final Bitmap bitmap) {
+
+        // Do the real work in an async task, because we need to use the network anyway
+        try {
+            AsyncTask<Object, Void, String> labelDetectionTask = new LableDetectionTask(this, prepareAnnotationRequest(bitmap));
+            labelDetectionTask.execute();
+        } catch (IOException e) {
+            Log.d(TAG, "failed to make API request because of other IOException " +
+                    e.getMessage());
+        }
+    }
+
+    private Vision.Images.Annotate prepareAnnotationRequest(Bitmap bitmap) throws IOException {
+        HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+        JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+        VisionRequestInitializer requestInitializer =
+                new VisionRequestInitializer(CLOUD_VISION_API_KEY) {
+                    /**
+                     * We override this so we can inject important identifying fields into the HTTP
+                     * headers. This enables use of a restricted cloud platform API key.
+                     */
+                    @Override
+                    protected void initializeVisionRequest(VisionRequest<?> visionRequest)
+                            throws IOException {
+                        super.initializeVisionRequest(visionRequest);
+
+                        String packageName = getPackageName();
+                        visionRequest.getRequestHeaders().set(ANDROID_PACKAGE_HEADER, packageName);
+
+                        String sig = PackageManagerUtils.getSignature(getPackageManager(), packageName);
+
+                        visionRequest.getRequestHeaders().set(ANDROID_CERT_HEADER, sig);
+                    }
+                };
+
+        Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+        builder.setVisionRequestInitializer(requestInitializer);
+
+        Vision vision = builder.build();
+
+        BatchAnnotateImagesRequest batchAnnotateImagesRequest =
+                new BatchAnnotateImagesRequest();
+        batchAnnotateImagesRequest.setRequests(new ArrayList<AnnotateImageRequest>() {{
+            AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+
+            // Add the image
+            Image base64EncodedImage = new Image();
+            // Convert the bitmap to a JPEG
+            // Just in case it's a format that Android understands but Cloud Vision
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+            byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+            // Base64 encode the JPEG
+            base64EncodedImage.encodeContent(imageBytes);
+            annotateImageRequest.setImage(base64EncodedImage);
+
+            // add the features we want
+            annotateImageRequest.setFeatures(new ArrayList<Feature>() {{
+                Feature labelDetection = new Feature();
+                labelDetection.setType("TEXT_DETECTION");
+                labelDetection.setMaxResults(MAX_LABEL_RESULTS);
+                add(labelDetection);
+            }});
+
+            // Add the list of one thing to the request
+            add(annotateImageRequest);
+        }});
+
+        Vision.Images.Annotate annotateRequest =
+                vision.images().annotate(batchAnnotateImagesRequest);
+        // Due to a bug: requests to Vision API containing large images fail when GZipped.
+        annotateRequest.setDisableGZipContent(true);
+        Log.d(TAG, "created Cloud Vision request object, sending request");
+
+        return annotateRequest;
+    }
+
+    private static class LableDetectionTask extends AsyncTask<Object, Void, String> {
+        private final WeakReference<ImageViewActivity> mActivityWeakReference;
+        private Vision.Images.Annotate mRequest;
+
+        LableDetectionTask(ImageViewActivity activity, Vision.Images.Annotate annotate) {
+            mActivityWeakReference = new WeakReference<>(activity);
+            mRequest = annotate;
+        }
+
+        @Override
+        protected String doInBackground(Object... params) {
+            try {
+                Log.d(TAG, "created Cloud Vision request object, sending request");
+                BatchAnnotateImagesResponse response = mRequest.execute();
+                return convertResponseToString(response);
+
+            } catch (GoogleJsonResponseException e) {
+                Log.d(TAG, "failed to make API request because " + e.getContent());
+            } catch (IOException e) {
+                Log.d(TAG, "failed to make API request because of other IOException " +
+                        e.getMessage());
+            }
+            return "Cloud Vision API request failed. Check logs for details.";
+        }
+
+        protected void onPostExecute(String result) {
+            ImageViewActivity activity = mActivityWeakReference.get();
+            if (activity != null && !activity.isFinishing()) {
+                TextView imageDetail = activity.findViewById(R.id.text_view);
+                imageDetail.setText(result);
+            }
+        }
+    }
+
+    private static String convertResponseToString(BatchAnnotateImagesResponse response) {
+        StringBuilder message = new StringBuilder("読み取り結果は以下の通りです。:\n\n");
+
+        TextAnnotation label = response.getResponses().get(0).getFullTextAnnotation();
+        if (label != null) {
+            message.append(label.getText());
+        } else {
+            message.append("nothing");
+        }
+
+        return message.toString();
+    }
+
+    private Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
     }
 
     /* Checks if external storage is available to at least read */
@@ -83,35 +270,19 @@ public class ImageViewActivity extends AppCompatActivity {
     }
 
     protected void findViews(){
-        button1 = (Button)findViewById(R.id.button1);
-        imageView1 = (ImageView)findViewById(R.id.imageView1);
+        button1 = findViewById(R.id.button1);
+        imageView1 = findViewById(R.id.imageView1);
     }
 
     protected void setListeners(){
-        button1.setOnClickListener(new OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                // 画面遷移用のintentを作成する。基本、「Intent(前の画面, 後の画面)」で書かれる。前の画面はgetApplication();でいいんじゃないか?
-                // でも、CameraActivityは実質的にはCamera2BasicFragmentを動かしている。よって、ここに戻るためには、Fragment⇒Activityを行うことになる。
-                Intent intent = new Intent(
-                        getApplication(), CameraActivity.class);
-                startActivityForResult(
-                        intent,
-                        REQUEST_CAPTURE_IMAGE);
-            }
+        button1.setOnClickListener(v -> {
+            // 画面遷移用のintentを作成する。基本、「Intent(前の画面, 後の画面)」で書かれる。前の画面はgetApplication();でいいんじゃないか?
+            // でも、CameraActivityは実質的にはCamera2BasicFragmentを動かしている。よって、ここに戻るためには、Fragment⇒Activityを行うことになる。
+            Intent intent = new Intent(
+                    getApplication(), CameraActivity.class);
+            startActivityForResult(
+                    intent,
+                    REQUEST_CAPTURE_IMAGE);
         });
-    }
-
-    @Override
-    protected void onActivityResult(
-            int requestCode,
-            int resultCode,
-            Intent data) {
-        if(REQUEST_CAPTURE_IMAGE == requestCode
-                && resultCode == Activity.RESULT_OK ){
-            Bitmap capturedImage =
-                    (Bitmap) data.getExtras().get("data");
-            imageView1.setImageBitmap(capturedImage);
-        }
     }
 }
