ffd635ab464d2af7ee93bdf3641662cdc0841522
soundofhorizon <ooshiba0206@gmail.com>
2021-09-01 18:23:16 +0900

    [FIX]オーバーレイの枠の縦横比を1:2にする

--- Application/src/main/java/com/example/android/ProjectDTeamA2Application/overlay.java
+++ Application/src/main/java/com/example/android/ProjectDTeamA2Application/overlay.java
@@ -1,11 +1,18 @@
 package com.example.android.ProjectDTeamA2Application;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class overlay extends View {
     private Paint paint = new Paint();
@@ -21,9 +28,19 @@ public class overlay extends View {
         paint.setColor(Color.GREEN);
         paint.setStrokeWidth(10);
 
-        //center
-        canvas.drawRect(50, 500, 1050, 1000, paint);
+        WindowManager wm = (WindowManager)this.getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = Objects.requireNonNull(wm).getDefaultDisplay();
+        @SuppressLint("DrawAllocation") DisplayMetrics displayMetrics = new DisplayMetrics();
+        display.getMetrics(displayMetrics);
 
+
+        //center
+        canvas.drawRect(display.getWidth()/2 -500, display.getHeight()/2 -250 - dp2px(112, this.getContext()), 500 + display.getWidth() / 2, display.getHeight()/2 + 250 - dp2px(112, this.getContext()), paint);
+
+    }
+
+    public int dp2px(float dp,Context context){
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp,context.getResources().getDisplayMetrics());
     }
 
 
